33e160ca1a8a69a1408d87d9d655a8d9952cb0f0
Guilherme Mossi Bento <guilhermemossibento@gmail.com>
2024-07-25 00:35:56 -0400

    change task

--- tasks.py
+++ tasks.py
@@ -1,43 +1,25 @@
+# tasks.py
 from celery import Celery
-from billiard import cpu_count
+from billiard import Pool, cpu_count
+import math
 
 app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
-def calculate_primes(limit):
-    def is_prime(n):
-        if n <= 1:
-            return False
-        if n <= 3:
-            return True
-        if n % 2 == 0 or n % 3 == 0:
-            return False
-        i = 5
-        while i * i <= n:
-            if n % i == 0 or n % (i + 2) == 0:
-                return False
-            i += 6
-        return True
 
-    primes = []
-    for num in range(2, limit):
-        if is_prime(num):
-            primes.append(num)
-    return primes
+def cpu_intensive_workload(start, end):
+    x = 0
+    for i in range(start, end):
+        x += math.sqrt(i)
+    return x
+
 
 @app.task
-def cpu_intensive_task(limit):
+def cpu_intensive_task(n):
     num_cpus = cpu_count()
-    chunk_size = limit // num_cpus
+    chunk_size = n // num_cpus
     ranges = [(i * chunk_size, (i + 1) * chunk_size) for i in range(num_cpus)]
 
-    def compute_chunk(start, end):
-        return calculate_primes(end)  # Compute primes in the given range
-
-    from billiard import Pool
     with Pool(processes=num_cpus) as pool:
-        results = pool.starmap(compute_chunk, ranges)
+        results = pool.starmap(cpu_intensive_workload, ranges)
 
-    # Flatten the results list and count primes
-    all_primes = [prime for sublist in results for prime in sublist]
-    total_primes = len(all_primes)
-    return total_primes
+    return sum(results)
--- trigger_task.py
+++ trigger_task.py
@@ -1,17 +1,8 @@
+# trigger_task.py
 from tasks import cpu_intensive_task
 
-
-def trigger_tasks():
-    limit = 10 ** 4  # Set the limit for prime number calculation
-    num_tasks = 1  # Number of tasks to trigger
-    for _ in range(num_tasks):
-        result = cpu_intensive_task.delay(limit)
-        print(f"Task ID: {result.id}")
-
-        # Wait for the result and print the total number of primes
-        total_primes = result.get(timeout=20)  # Wait up to 1 hour for the result
-        print(f"Total number of primes found: {total_primes}")
-
-
 if __name__ == "__main__":
-    trigger_tasks()
+    num_tasks = 100  # Adjust this number as needed
+    task_size = 10**100  # Increase this number to make the task more CPU-intensive
+    for _ in range(num_tasks):
+        cpu_intensive_task.delay(task_size)
