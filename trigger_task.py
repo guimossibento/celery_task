178b3e5a6ac355bff311b38212cc47d73428afda
Guilherme Mossi Bento <guilhermemossibento@gmail.com>
2024-07-25 01:16:45 -0400

    change task

--- trigger_task.py
+++ trigger_task.py
@@ -2,7 +2,7 @@
 from tasks import cpu_intensive_task
 
 if __name__ == "__main__":
-    num_tasks = 2  # Adjust this number as needed
+    num_tasks = 4  # Adjust this number as needed
     task_size = 500000000  # Increase this number to make the task more CPU-intensive
     for _ in range(num_tasks):
         cpu_intensive_task.delay(task_size)
