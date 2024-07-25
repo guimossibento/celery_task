24520d04acc828d85793779e6bef92a05d6ae17a
Guilherme Mossi Bento <guilhermemossibento@gmail.com>
2024-07-25 00:45:56 -0400

    change task

--- trigger_task.py
+++ trigger_task.py
@@ -6,9 +6,3 @@ if __name__ == "__main__":
     task_size = 10**6  # Increase this number to make the task more CPU-intensive
     for _ in range(num_tasks):
         cpu_intensive_task.delay(task_size)
-
-git pull origin master
-docker stack rm celery_stack
-docker build -t localhost:5000/celery_worker:latest .
-docker push localhost:5000/celery_worker:latest
-docker stack deploy -c docker-compose.yml celery_stack
